8028773cbfeb8a15998b52194538c1505f379bb3
AndresRCal <aareyesc@liverpool.com.mx>
2016-11-09 16:19:44 -0600

    comentar columna ADM

--- untitled0.py
+++ untitled0.py
@@ -13,8 +13,8 @@ df_sentimiento,df_intensificadores,df_negadores,S_negation_breaks=catalogs.Impor
 df_negocio,df_layout1=impex.importar_excel_negocio()
 
 Layout_Sentimiento=pd.merge(df_layout1,df_sentimiento,left_on='PALABRA_LIMPIA',right_on='palabra',how='left')
-Layout_ADM = enc.encuentra_admiracion(Layout_Sentimiento,df_negocio)
-Layout_ADM=Layout_ADM[['EVENT_ID_NO', 'PALABRA_LIMPIA', 'PALABRA_ORIGINAL','POSICION','ADM']]
+#Layout_ADM = enc.encuentra_admiracion(Layout_Sentimiento,df_negocio)
+#Layout_ADM=Layout_ADM[['EVENT_ID_NO', 'PALABRA_LIMPIA', 'PALABRA_ORIGINAL','POSICION','ADM']]
 Layout_Sentimiento=Layout_Sentimiento[['EVENT_ID_NO', 'PALABRA_LIMPIA', 'PALABRA_ORIGINAL', 'POSICION','valor']]
 Layout_Sentimiento=Layout_Sentimiento.fillna(0)
 Layout_Sentimiento.rename(columns={'valor':'SENTIMIENTO_PALABRA'}, inplace=True)
@@ -31,6 +31,6 @@ Layout_Negadores.rename(columns={'valor':'Negadores'}, inplace=True)
 
 Merg1=pd.merge(Layout_Sentimiento,Layout_Intensificador,on=['EVENT_ID_NO','PALABRA_LIMPIA','PALABRA_ORIGINAL','POSICION'],how='left')
 Merg2=pd.merge(Merg1,Layout_Negadores,on=['EVENT_ID_NO','PALABRA_LIMPIA','PALABRA_ORIGINAL','POSICION'],how='left')
-Merg3=pd.merge(Merg2,Layout_ADM,on=['EVENT_ID_NO','PALABRA_LIMPIA','PALABRA_ORIGINAL','POSICION'],how='left')
-Merg4=enc.columna_breaks(Merg3,S_negation_breaks)
+#Merg3=pd.merge(Merg2,Layout_ADM,on=['EVENT_ID_NO','PALABRA_LIMPIA','PALABRA_ORIGINAL','POSICION'],how='left')
+Merg4=enc.columna_breaks(Merg2,S_negation_breaks)
 
