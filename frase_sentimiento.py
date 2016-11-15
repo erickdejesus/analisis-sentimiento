1ab6129f10bbfff2f67186339fd333d48e11ec23
erickdejesus <edejc@liverpool.com.mx>
2016-11-15 16:02:41 -0600

    syn 15112016

---
+++ frase_sentimiento.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 18:01:18 2016
+
+@author: edejc
+"""
+
+def encuentra_modificadores(entrada,negadores,sentimiento, n_breaks, intensificadores):
+    print(entrada[1])
+    ax = sentimiento[sentimiento['palabra'] = entrada[1]]
+     
+    salida=ax
+    return salida
+
+def list_sentiment(palabra):
+ 
+    
+    return
--- run_ejc.py
+++ run_ejc.py
@@ -9,13 +9,17 @@ import pandas as pd
 import func_import_catalogos as catalogs
 import importar_excel as impex
 import funciones_varias as enc
+import frase_sentimiento as fr
 
 df_sentimiento,df_intensificadores,df_negadores,S_negation_breaks=catalogs.Import_Catalogs()
 df_negocio,df_layout1=impex.importar_excel_negocio()
 
-Layout_Sentimiento=pd.merge(df_layout1,df_sentimiento,left_on='PALABRA_LIMPIA',right_on='palabra',how='left')
+exit1 = fr.encuentra_modificadores(df_negocio['MENSAJE_SPL'][105],df_negadores,df_sentimiento,S_negation_breaks,df_intensificadores)
+#entrada,negadores,sentimiento, n_breaks, intensificadores
+print (exit1)
+#Layout_Sentimiento=pd.merge(df_layout1,df_sentimiento,left_on='PALABRA_LIMPIA',right_on='palabra',how='left')
 
-salida_pb1 = enc.find_sentiment_phrase(df_negocio,df_sentimiento)
+#salida_pb1 = enc.find_sentiment_phrase(df_negocio,df_sentimiento)
 
 ##Layout_ADM = enc.encuentra_admiracion(Layout_Sentimiento,df_negocio)
 ##Layout_ADM=Layout_ADM[['EVENT_ID_NO', 'PALABRA_LIMPIA', 'PALABRA_ORIGINAL','POSICION','ADM']]
