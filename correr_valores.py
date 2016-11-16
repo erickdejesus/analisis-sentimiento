5cc30d750bc637f8674d8d592e9f4c28d70262f5
AndresRCal <aareyesc@liverpool.com.mx>
2016-11-16 12:49:17 -0600

    Funcion correr valores.V0
    promer bosquejo de correr valores

--- correr_valores.py
+++ correr_valores.py
@@ -9,10 +9,43 @@ import pandas as pd
 import numpy as np
 import funciones_varias as enc
 
-def corre_valor_intensificador(x1):
+#==============================================================================
+#     FUNCIONES DE AYUDA
+#==============================================================================
+def hay_mayor_q_A_en_B(valorX,list_romp):
+    cnt=0
+    for i in list_romp:
+        if i > valorX:
+            cnt=cnt+1
+    return(int(cnt))
+    
+def siguiente_mayor_n_B(valorX,list_romp):
+    valsM=[]
+    for i in list_romp:
+        if i > valorX:
+            valsM+=[i]
+    return(int(valsM[0]))
+        
+def hay_menor_q_A_en_B(valorX,list_romp):
+    cnt=0
+    for i in list_romp:
+        if i < valorX:
+            cnt=cnt+1
+    return(int(cnt))
+    
+def siguiente_menor_n_B(valorX,list_romp):
+    valsM=[]
+    for i in list_romp:
+        if i < valorX:
+            valsM+=[i]
+    return(valsM[len(valsM)-1])
+    
+#==============================================================================
+#         FUNCION PRINCIPAL
+#==============================================================================
+
+def correr_valor_multiplicador(x1):
     x1=enc.cambiar_formato_numero(x1)
-#    pos_init=x1['POSICION'].min() 
-#    pos_fin=x1['POSICION'].max()
     list_romp=x1['POSICION'][x1['ROMPEDOR_F']>0]
     list_romp=list(list_romp)
     list_intns=x1['POSICION'][x1['INTENSIFICADOR']>0]
@@ -21,21 +54,80 @@ def corre_valor_intensificador(x1):
     list_neg=list(list_neg)
     list_ADM=x1['POSICION'][x1['ADM']!=0]
     list_ADM=list(list_ADM)
-#    val_aux=0
+    
+#     CORRER INTENSIFICADORES
+
     if len(list_intns) > 0 and len(list_romp) > 0:
-        band=0
-        for i in list_romp:
-            print(i)
-            print(int(list_intns[0]))
-            print(int(list_romp[band]))
-            if int(list_intns[0]) < int(list_romp[band]):
-                for j in range(int(list_intns[0]),int(list_romp[band])):
-                    print(j)
-                    x1.loc[x1['POSICION']>=j,'INTENSIFICADOR']=float(x1['INTENSIFICADOR'][x1['POSICION']==list_intns[0]])
-                    band=band+1
+        for i in list_intns:
+            CntMys=hay_mayor_q_A_en_B(i,list_romp)
+            if CntMys > 0:
+                pos_init=int(i)
+                pos_fin=siguiente_mayor_n_B(i,list_romp)
+                x1.iloc[int(pos_init):int(pos_fin)+1,5]=float(x1['INTENSIFICADOR'][x1['POSICION'] == pos_init])
             else:
-                band=band+1
-                continue
-    if len(list_romp) == 0:
-        x1.ix[x1.POSICION >= list_intns[0],'INTENSIFICADOR'] = float(x1['INTENSIFICADOR'][x1['POSICION']==list_intns[0]])
+                pos_init=int(i)
+                x1.ix[x1.POSICION >= pos_init,'INTENSIFICADOR'] = float(x1['INTENSIFICADOR'][x1['POSICION'] == pos_init])
+    elif len(list_intns) > 0 and len(list_romp) == 0:
+        if len(list_intns) == 1:
+            pos_init=int(list_intns[0])
+            x1.ix[x1.POSICION >= pos_init,'INTENSIFICADOR'] = float(x1['INTENSIFICADOR'][x1['POSICION'] == pos_init])
+        else:
+            for i in list_intns:
+                pos_init=int(i)
+                x1.ix[x1.POSICION >= pos_init,'INTENSIFICADOR'] = float(x1['INTENSIFICADOR'][x1['POSICION'] == pos_init])
+    else: 
+        pass
+
+#     CORRER SIGNOS DE ADMIRACION!
+
+    if len(list_ADM) > 0 and len(list_romp) > 0:
+        for i in list_ADM:
+            CntMns=hay_menor_q_A_en_B(i,list_romp)
+            if CntMns > 0:
+                pos_init=siguiente_menor_n_B(i,list_romp)
+                pos_fin=int(i)
+                x1.iloc[int(pos_init):int(pos_fin),7]=float(x1['ADM'][x1['POSICION'] == pos_fin])
+            else:
+                pos_init=0
+                pos_fin=int(i)
+                x1.iloc[int(pos_init):int(pos_fin),7]=float(x1['ADM'][x1['POSICION'] == pos_fin])
+    elif len(list_ADM) > 0 and len(list_romp) == 0:
+        if len(list_intns) == 1:
+            pos_init=0
+            pos_fin=int(list_ADM[0])
+            x1.iloc[int(pos_init):int(pos_fin),7]=float(x1['ADM'][x1['POSICION'] == pos_fin])
+        else:
+#                hay que trabajar en este caso
+             pos_init=0
+             pos_fin=int(i)
+             x1.iloc[int(pos_init):int(pos_fin),7]=float(x1['ADM'][x1['POSICION'] == pos_fin])
+    else:
+        pass
+    
+#     CORRER Negadores
+    
+    if len(list_neg) > 0 and len(list_romp) > 0:
+        for i in list_neg:
+            CntMys=hay_mayor_q_A_en_B(i,list_romp)
+            if CntMys > 0:
+                pos_init=int(i)
+                pos_fin=siguiente_mayor_n_B(i,list_romp)
+                x1.iloc[int(pos_init):int(pos_fin)+1,6]=float(x1['Negadores'][x1['POSICION'] == pos_init])
+            else:
+                pos_init=int(i)
+                x1.ix[x1.POSICION >= pos_init,'Negadores'] = float(x1['Negadores'][x1['POSICION'] == pos_init])
+    elif len(list_neg) > 0 and len(list_romp) == 0:
+        if len(list_neg) == 1:
+            pos_init=int(list_neg[0])
+            x1.ix[x1.POSICION >= pos_init,'Negadores'] = float(x1['Negadores'][x1['POSICION'] == pos_init])
+        else:
+            for i in list_neg:
+                pos_init=int(i)
+                x1.ix[x1.POSICION >= pos_init,'Negadores'] = float(x1['Negadores'][x1['POSICION'] == pos_init])
+    else: 
+        pass
+    
+#    Calculo de columna RESULTADO_DESMENUZADO
+
     return(x1)
+    
